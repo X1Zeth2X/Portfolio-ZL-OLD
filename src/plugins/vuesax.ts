0d373afed2767c07927ca0e0db52b3ffa4909cb4
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-08 20:00:51 +0400

    Switched to normal use.

--- src/plugins/vuesax.ts
+++ src/plugins/vuesax.ts
@@ -1,24 +1,6 @@
 import Vue from "vue";
-import {
-  vsButton,
-  vsCard,
-  vsIcon,
-  vsNavbar,
-  vsDivider,
-  vsChip,
-  vsAvatar,
-  vsRow,
-  vsCol
-} from "vuesax";
+import VueSax from "vuesax";
 
 import "vuesax/dist/vuesax.css";
 
-Vue.use(vsButton);
-Vue.use(vsCard);
-Vue.use(vsNavbar);
-Vue.use(vsIcon);
-Vue.use(vsChip);
-Vue.use(vsDivider);
-Vue.use(vsAvatar);
-Vue.use(vsRow);
-Vue.use(vsCol);
+Vue.use(VueSax);
