9f16c8cd9de6d32f63ddea071779ab90e9682920
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-10 20:42:02 +0400

    Basic stuff.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about tc">
-    <h1>You'll know more about me soon!</h1>
+  <div class="pt5 tc">
+    <section id="intro" class="grey">
+      <h1 class="f1-l f2 b">Zeth Magno Leonardo</h1>
+      <p class="f2-l f2-m f3 w-60-m w-60-l w-90 desc">
+        I had nothing interesting to write here at the time,
+        so just think of this as a pretty interesting introduction 🤔
+      </p>
+
+      <div class="mt4 f3">
+        <vs-button
+          size="large"
+          type="relief"
+          class="btn-radius fw7"
+          color="#F18D9E"
+        >Get in contact</vs-button>
+      </div>
+    </section>
   </div>
 </template>
